ca9604c524d18127c91dea49d8dd9a11db730d0f
Dan Spinosa <spinosa@gmail.com>
2013-03-29 15:41:03 -0400

    put the gesture recognizer comments in the .h where they belong

--- BoredNoMore/DSBoredGestureRecognizer.m
+++ BoredNoMore/DSBoredGestureRecognizer.m
@@ -2,22 +2,6 @@
 //  DSBoredGestureRecognizer.m
 //  Built at Shelby.tv in NYC
 //
-//  The gesture recognizer Apple forgot.  Maybe they don't get bored, but maybe your users do.
-//  Finally, you can detect boredom and fix it by showing pictures of Unircorns (or whatever
-//  floats your boat).
-//
-//  The DSBoredGestureRecognizer looks for the classic waterfall from pinky to index finger.  By default,
-//  it waits for two of these sequences before signaling recognized.  You can customize the number of
-//  required sequences with -[DSBoredGestureRecognizer setNumberOfSequencesRequired:(NSUInteger)].
-//
-//  Want to detect that a user is more impatient than bored?  You can also reduce the grace period allotted
-//  between waterfall sequences via -[DSBoredGestureRecognizer setGraceBetweenSequences:(NSTimeInterval)].
-//
-//  Accessiblity is important.  If your users have more or fewer fingers, adjust the minimum number of fingers
-//  required in the waterfall with -[DSBoredGestureRecognizer setNumberOfTouchesRequired:(NSUInteger)];
-//
-//  PS You should attach this gesture to a full window root view.  Want to know why?  Read the code and comments.
-//
 //  Created by Daniel Spinosa on 3/28/13.
 //  Copyright (c) 2013 Daniel Spinosa. All rights reserved.
 //
